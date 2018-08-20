c2aa9276621272ef6ca29d5d080705f0a2cf69f9
mohyneenm <mohyneenm@yahoo.com>
2018-08-20 00:30:33 -0400

    Add files via upload

---
+++ SequencePrediction/SequencePrediction.py
@@ -0,0 +1,8 @@
+
+from CPT import *
+model = CPT()
+#data,target = model.load_files("./data/train_intents.csv","./data/test_intents.csv")
+data,target = model.load_files("./data/train_extracols.csv","./data/test2.csv")
+model.train(data)
+predictions = model.predict(data,target,3,1)
+print(predictions)
